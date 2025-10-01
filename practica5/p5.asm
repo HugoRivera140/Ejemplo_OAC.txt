7c744320d77793469a2f65cb076744245fe3b589
HugoRivera140 <127377794+HugoRivera140@users.noreply.github.com>
2025-10-01 20:17:56 +0000

    cambios p5

--- practica5/p5.asm
+++ practica5/p5.asm
@@ -4,41 +4,42 @@ section .text
     global _start:
 
 _start:
-    MOV EDX, msg
-    CALL puts
+    MOV EDX, msg           ;Se manda el inicio de la cadena a EDX
+    CALL puts              ;Se llama a put e imprime la cadena
     CALL salto
 
-    MOV EDI, 0
-    MOV ESI, 0
-    MOV EBX, cad
+    MOV EDI, 0             ;Se reinician los valores de ESI y EDI para  
+    MOV ESI, 0             ;trabajar con ellos
+    MOV EBX, cad           ;Se pone el inicio de la variable cad en EBX
 
     capturar:
-        CALL getche
-        CMP AL, '*'
+        CALL getche        ;Se Captura y muestra un caracter, se guarda en AL
+        CMP AL, '*'        ;Se compara el valor del caracter introducido y se sale si es '*'
         JE salir
 
-        MOV [EBX+EDI], AL
-        INC EDI
+        MOV [EBX+EDI],AL   ;Si es diferente a '*' agrega el caracter a [EBX+EDI] que es la posición de cad
+        INC EDI            ;Se incrementa el valor de EDI que sirve como indice para modificar 
     JMP capturar
 
     ;imprimir cadena capturada
     salir:
-        MOV EBX, change
-        MOV byte[EBX], '%'
-        MOV AL, [EBX]
+        MOV EBX, change    ;Change es una variable temporal para cambios
+        MOV byte[EBX],'%'  ;Se agrega el simbolo '%' a EBX 
+        MOV AL, [EBX]      ;El valor de EBX se pasa a AL
 
-        MOV EBX, cad
-        MOV [EBX+EDI], AL
-        
-        MOV EDX, cad
+        MOV EBX, cad       ;Se coloca la dirección de cad en EBX
+        MOV [EBX+EDI], AL  ;En la posicion [EBX+EDI] (posicion donde se terminó el ciclo) 
+                           ;se coloca lo que hay en AL ('%')
+
+        MOV EDX, cad       ;Se manda la dirección de cad a EDX y se muestra al llamar a puts
         CALL salto
         CALL puts
         CALL salto
 
-        CALL newputs
+        CALL newputs       ;Se llama a newputs (nueva funcion de imprimir)
         CALL salto
 
-    CALL palindromo
+    CALL palindromo        ;Se llama la funcion palindromo (en EBX la dirección de la cadena)
 
     mov eax, 1
     mov ebx, 0
@@ -49,15 +50,15 @@ _start:
 ; edx pasar direccion base
 newputs:
     pushad
-    MOV ESI, 0
+    MOV ESI, 0              ;Se reinicia el valor de ESI
     captPuts:
-        MOV AL, [EDX+ESI]
-        CMP al, '%'
-        je end
+        MOV AL, [EDX+ESI]   ;En AL se coloca el valor de la cadena en la posición ESI
+        CMP al, '%'         ;Se compara para ver si es '%' (final de cadena)
+        je end              ;Salta a end si se encontro el simbolo '%'
 
-        INC ESI
-        CALL putchar
-    jmp captPuts
+        INC ESI             ;Si no se encontro se incrementa ESI 
+        CALL putchar        ;Se imprime el carácter que se encuentra en la posición actual
+    jmp captPuts            ;Regresa al ciclo en la siguiente posición
 
     end:    
         popad
@@ -66,42 +67,42 @@ newputs:
 
 palindromo:
     PUSHAD
-    DEC EDI
-    MOV ESI, 0
+    DEC EDI                 ;Se le resta uno a EDI (actualmente está en el simbolo agregado)
+    MOV ESI, 0              ;Se reinicia ESI ppara usarlo
 
     regreso:
-        MOV AL, [EBX+ESI]
-        CMP AL, [EBX+EDI]
-        JE next
+        MOV AL, [EBX+ESI]   ;El valor de [EBX+ESI] se coloca en AL
+        CMP AL, [EBX+EDI]   ;Se compara con el valor de [EBX+EDI] 
+        JE next             ;Si los carácteres son iguales sigue siendo palindromo y salta a next
 
-        JMP noPal
+        JMP noPal           ;Si fueron diferentes salta a noPal ya que no fue palindromo
 
 
     next:
-        CMP ESI, EDI
-        JE siPal
+        CMP ESI, EDI        ;Se comparan ESI y EDI para ver si estan en la misma posición
+        JE siPal            ;Cierra el si llegó al final de las comparaciones
 
 
-        INC ESI
-        CMP ESI, EDI
-        JE siPal
+        INC ESI             ;Se incrementa ESI para pasar a la siguiente posición
+        CMP ESI, EDI        ;Se vuelven a comparar para ver si se llegó al centro de la palabra
+        JE siPal            ;Cierra el ciclo si se llegó a la mitad
 
-        DEC EDI
-        JMP regreso
+        DEC EDI             ;Se le resta uno a EDI para que se comparen las siguientes posiciones
+        JMP regreso         ;Se regresa al incio del ciclo
 
     siPal:
-        MOV EDX, valido
-        CALL newputs
+        MOV EDX, valido     ;Si fue palindromo se pone la dirección del mensaje 'valido' a EDX
+        CALL newputs        ;Se llama a newputs y se imprime la cadena
         CALL salto
         POPAD
-        RET
+        RET                 ;Se regresa a cuando se llamó la etiqueta
 
     noPal:
-        MOV EDX, invalido
-        CALL newputs
+        MOV EDX, invalido   ;Si no fue palindromo se pone la dirección del mensaje 'invalido' a EDX
+        CALL newputs        ;Se llama a newputs y se imprime la cadena
         CALL salto
         POPAD
-        RET
+        RET                 ;Se regresa a cuando se llamó la etiqueta
 
 salto:
   pushad
