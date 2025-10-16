617b38ec90864ddc723e5fa0cbe2b20d9b643023
HugoRivera140 <127377794+HugoRivera140@users.noreply.github.com>
2025-10-16 18:14:31 +0000

    practica8 comentada

--- practica8/p8.asm
+++ practica8/p8.asm
@@ -4,113 +4,112 @@ section .text
     global _start:
 
 _start: 
-    MOV AX, 0
-    MOV ESI, 0
+    MOV AX, 0               ;Se pone 0 en AX y en ESI
+    MOV ESI, 0              
 
     capturar:
-        CALL getche
+        CALL getche         ;Se captura un caracter, se muestra y se guarda en AL
         
-        CMP AL, '*'
-        JE endCapt
+        CMP AL, '*'         ;Se compara para ver si el caracter ingresado es el final de cadena
+        JE endCapt          ;Se salta a endCapt si fue '*' el carácter ingresado
 
-        PUSH AX
-        INC ESI 
-        JMP capturar
+        PUSH AX             ;Se hace PUSH a AX para ir ingresando el carácter
+        INC ESI             ;Se incrementa ESI que llevará la cantidad de carácteres
+        JMP capturar        ;Se regresa a capturar para la siguiente posición del carácter
 
         endCapt: 
             CALL salto
 
-    MOV CX, SI
+    MOV CX, SI              ;Lo que hay en SI(ultima posición) se pone en CX para ir iterando
     imprimir:
-        POP AX
-        CALL putchar
-    loop imprimir
+        POP AX              ;Saca lo ultimo ingresado y lo guarda en AX
+        CALL putchar        ;Se imprime lo que hay en AL (último carácter sacado)
+    loop imprimir           ;Se repite para ir sacando todos los carácteres que se ingresaron
 
     CALL salto
 
 ;====================================
-    MOV AX, 0
-    MOV ESI, 0
+    MOV AX, 0                       ;Se pone AX en 0 otra vez
+    MOV ESI, 0                      ;Se pone ESI en 0 otra vez
 
-    MOV EBX, cadenaB
-    MOV byte[EBX], '0'
+    MOV EBX, cadenaB                ;Se pone el inicio de dirección de la cadenaB en EBX
+    MOV byte[EBX], '0'              ;Pongo un '0' en el contenido de EBX (direccion de inicio de cadenaB)
 
     capturarB:
-        CALL getche
+        CALL getche                 ;Se captura un carácter y se guarda en AL
         
-        CMP AL, '*'
-        JE endCaptB
+        CMP AL, '*'                 ;Se compara AL para ver si el carácter ingresado es el final de cadena ('*')
+        JE endCaptB                 ;Si se ingresó el carácter salta al fin de captura binaria
 
-        MOV [EBX+ESI], AL
-        INC ESI 
-        JMP capturarB
+        MOV [EBX+ESI], AL           ;Lo que haya en AL se mueve a la dirección [EBX+ESI] el cual es la posición actual de la cadena binaria
+        INC ESI                     ;Se incrementa ESI para la siguiente posición
+        JMP capturarB               ;Salta de regreso a capturar binaria
 
         endCaptB:
-            MOV byte[EBX+ESI], '%'
+            MOV byte[EBX+ESI], '%'  ;Se agrega '%' en la posición después de lo último ingresado
             CALL salto
 
-    CALL getche
-    SUB AL, '0'
+    CALL getche                     ;Se captura un carácter y se guarda en AL
+    SUB AL, '0'                     ;Se resta '0' para que sea su valor decimal y no ascii
     CALL salto
     CALL salto
 
-    MOV CL, AL
+    MOV CL, AL                      ;Se pone lo que hay en AL (número de corrimientos) en CL para usar el loop siguiente
 
 
-    MOV AX, 0
-    MOV EBX, cadenaB
+    MOV AX, 0                       ;Se pone AX en 0
+    MOV EBX, cadenaB                ;Se pone la dirección de cadena binaria en EBX
     
     corrimientos:
 
-        MOV AL, [EBX+2] 
-        MOV byte[EBX+3], AL
+        MOV AL, [EBX+2]             ;Lo que hay en la posición 2 de la cadena binaria se guarda en AL
+        MOV byte[EBX+3], AL         ;Lo que hay en AL se pone en la posición 3 de la cadena binaria
 
-        MOV AL, [EBX+1]
-        MOV byte[EBX+2], AL
+        MOV AL, [EBX+1]             ;Lo que hay en la posición 1 de la cadena binaria se guarda en AL
+        MOV byte[EBX+2], AL         ;Lo que hay en AL se pone en la posición 2 de la cadena binaria
 
-        MOV AL, [EBX+0]
-        MOV byte[EBX+1], AL
+        MOV AL, [EBX+0]             ;Lo que hay en la posición 0 de la cadena binaria se guarda en AL
+        MOV byte[EBX+1], AL         ;Lo que hay en AL se pone en la posición 1 de la cadena binaria
 
-        MOV byte[EBX+0], '0'
+        MOV byte[EBX+0], '0'        ;En la posición 0 de la cadena binaria se coloca un 0
 
-        loop corrimientos
+        loop corrimientos           ;Se hace el loop la cantidad de corrimientos que haya ingresado el usuario
 
 
-    MOV EDX, cadenaB
-    CALL newputs
+    MOV EDX, cadenaB                ;Se coloca la dirección de la cadena binaria en EDX
+    CALL newputs                    ;Se pone en EDX lo que se quiera imprimir y que termine en '%'
     CALL salto
 
 
-    MOV ESI, 3
-    MOV EDI, 0
-    MOV EAX, conversionB
-    MOV byte[EAX], '0'
+    MOV ESI, 3                      ;En ESI se coloca el 3 que es la última posición de la cadena binaria
+    MOV EDI, 0                      ;Se pone EDI en 0
+    MOV EAX, conversionB            ;En EAX Se coloca la dirección donde se va a guardar la conversión binaria
+    MOV byte[EAX], '0'              ;Se le coloca un 0 en el contenido de EAX (dirección base de conversión binaria)
 
-    MOV EDX, tabla
-    MOV CX, 4
+    MOV EDX, tabla                  ;En EDX se coloca la dirección de la tabla de números binarios
+    MOV CX, 4                       ;En CX se coloca un 4 ya que son 4 posiciones las que se deben revisar
 
     conversion:
-        MOV EBX, cadenaB
-        CMP byte[EBX+ESI], '1'
-        JE sumar
+        MOV EBX, cadenaB            ;Se pone en EBX la dirección de cadena binaria en cada iteración de la conversión
+        CMP byte[EBX+ESI], '1'      ;Se compara la posición ESI de la cadena binaria para ver si el carácter es un '1'
+        JE sumar                    ;Si fue 1 salta para sumar dependiendo de la posición de ese 1
 
         seguirC:
+            INC EDI                 ;Se incrementa EDI (posición de la tabla a revisar)
+            DEC ESI                 ;Se decrementa ESI (posición de la cadena binaria hacia atrás)
+            LOOP conversion         ;Se hace 4 veces el ciclo para cada una de las 4 posiciones de la cadena binaria
 
-        INC EDI
-        DEC ESI
-        LOOP conversion
+            JMP fin                 ;Al terminar el ciclo salta al fin de la subrutina
 
-        JMP fin
-
-        sumar: 
-            MOV EBX, [EDX+EDI]
-            ADD [EAX], EBX
-            JMP seguirC
+        sumar:                      ;Cuando el carácter revisado sea uno 
+            MOV EBX, [EDX+EDI]      ;En EBX se coloca lo que haya en [EDX+EDI] (valor de la tabla)  
+            ADD [EAX], EBX          ;Se le suma a EAX (conversion binaria) lo que haya en EBX (valor de la tabla)
+            JMP seguirC             ;Salta para seguir la conversión 
 
     fin:
-        MOV EDX, conversionB
-        MOV byte[EDX+1], '%'
-        CALL newputs
+        MOV EDX, conversionB        ;Al llegar al fin se pasa el valor que se fue sumando a EDX
+        MOV byte[EDX+1], '%'        ;Se le añade el carácter '%' para indicar final 
+        CALL newputs                ;Se llama a newputs e imprime el valor binario ya convertido
         CALL salto
 
 
@@ -122,7 +121,7 @@ _start:
 
 ;========================================================================
 
-;EDX pasar direccion base
+;EDX pasar direccion base que termine en '%'
 newputs:
     PUSHAD
     MOV ESI, 0              ;Se reinicia el valor de ESI
